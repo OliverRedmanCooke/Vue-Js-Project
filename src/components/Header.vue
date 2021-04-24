0af0dbf79bf6f222d2d1b807c663b8a5ca5e0ea3
OliverRedmanCooke <oliver.r.cooke@gmail.com>
2021-04-24 09:10:19 +0100

    First commit

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <h1>{{ title }}</h1>
+    <Button name='Add Task' />
+  </div>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "Hello World",
+    },
+  },
+  components: {Button}
+};
+</script>
+
+<style lang="scss" >
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
